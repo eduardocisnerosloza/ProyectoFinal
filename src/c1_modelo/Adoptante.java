7dfae7e2cfa6259a2240f7a78e74686fc0a61253
Esteban Cisneros <estebancis02@Estebans-MacBook-Pro.local>
2026-06-29 01:35:12 -0500

    v7

--- src/c1_modelo/Adoptante.java
+++ src/c1_modelo/Adoptante.java
@@ -4,20 +4,25 @@ public class Adoptante extends Persona {
     private double presupuestoMensual;
     private int horasFueraCasa;
     private boolean experienciaPrevia;
+    private boolean vetado;
 
     public Adoptante(String cedula, String nombre, double presupuestoMensual, int horasFueraCasa, boolean experienciaPrevia) {
         super(cedula, nombre);
         this.presupuestoMensual = presupuestoMensual;
         this.horasFueraCasa = horasFueraCasa;
         this.experienciaPrevia = experienciaPrevia;
+        this.vetado = false;
     }
 
     public double getPresupuestoMensual() { return presupuestoMensual; }
     public int getHorasFueraCasa() { return horasFueraCasa; }
     public boolean tieneExperiencia() { return experienciaPrevia; }
+    public boolean isVetado() { return vetado; }
+    public void setVetado(boolean vetado) { this.vetado = vetado; }
 
     @Override
     public String toString() {
-        return getNombre() + " (C.I: " + getCedula() + ")";
+        String alerta = vetado ? " [VETADO]" : "";
+        return getNombre() + " (C.I: " + getCedula() + ")" + alerta;
     }
 }
